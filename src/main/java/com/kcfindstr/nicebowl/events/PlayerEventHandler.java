1eef0c282fd74bc3526ae2da53ce1978a33fff67
KCFindstr <792491320@qq.com>
2021-12-17 22:34:35 +0800

    Add estrus effect

--- src/main/java/com/kcfindstr/nicebowl/events/PlayerEventHandler.java
+++ src/main/java/com/kcfindstr/nicebowl/events/PlayerEventHandler.java
@@ -1,20 +1,29 @@
 package com.kcfindstr.nicebowl.events;
 
+import com.kcfindstr.nicebowl.blocks.JuiceBlock;
 import com.kcfindstr.nicebowl.items.ItemRegistry;
 import com.kcfindstr.nicebowl.items.NiceBowl;
+import com.kcfindstr.nicebowl.potions.EffectRegistry;
+import com.kcfindstr.nicebowl.potions.EstrusEffect;
 import com.kcfindstr.nicebowl.utils.AdvancementUtils;
 import com.kcfindstr.nicebowl.utils.Constants;
 import com.kcfindstr.nicebowl.utils.PlayerData;
 import com.kcfindstr.nicebowl.utils.PlayerUtils;
 import com.kcfindstr.nicebowl.utils.Utils;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
@@ -86,20 +95,70 @@ public class PlayerEventHandler {
     if (source == null || !source.isProjectile()) {
       return;
     }
-    int niceBowlCount = 0;
-    if (entity.getItemBySlot(EquipmentSlotType.HEAD).getItem() instanceof NiceBowl) {
-      niceBowlCount++;
-    }
-    if (entity.getItemBySlot(EquipmentSlotType.LEGS).getItem() instanceof NiceBowl) {
-      niceBowlCount++;
-    }
-    if (niceBowlCount >= 2) {
-      event.setAmount(0);
-      if (entity instanceof ServerPlayerEntity) {
-        ServerPlayerEntity player = (ServerPlayerEntity) entity;
-        AdvancementUtils.grantAdvancement(player, AdvancementUtils.BLOCK_PROJECTILE);
-      }
+    float amount = event.getAmount();
+    if (amount <= 0) {
       return;
+    }
+    if (entity.hasEffect(EffectRegistry.estrus.get())) {
+      entity.heal(amount);
+      amount = 0;
+    } else {
+      int niceBowlCount = 0;
+      if (entity.getItemBySlot(EquipmentSlotType.HEAD).getItem() instanceof NiceBowl) {
+        niceBowlCount++;
+      }
+      if (entity.getItemBySlot(EquipmentSlotType.LEGS).getItem() instanceof NiceBowl) {
+        niceBowlCount++;
+      }
+      if (niceBowlCount >= 2) {
+        amount /= 4f;
+        if (entity instanceof ServerPlayerEntity) {
+          ServerPlayerEntity player = (ServerPlayerEntity) entity;
+          AdvancementUtils.grantAdvancement(player, AdvancementUtils.BLOCK_PROJECTILE);
+        }
+      }
+    }
+    event.setAmount(amount);
+  }
+
+  @SubscribeEvent
+  public static void onPlayerTick(PlayerTickEvent event) {
+    PlayerEntity player = event.player;
+    if (player.level.isClientSide) {
+      return;
+    }
+    BlockPos pos = new BlockPos(player.getPosition(0));
+    BlockState blockState = player.level.getBlockState(pos);
+    if (blockState.getBlock() instanceof JuiceBlock) {
+      Effect effect = EffectRegistry.estrus.get();
+      EffectInstance instance = player.getEffect(effect);
+      if (instance == null || instance.getDuration() <= EstrusEffect.EFFECT_INTERVAL) {
+        player.addEffect(new EffectInstance(effect, EstrusEffect.EFFECT_INTERVAL * 2));
+      }
+    }
+  }
+
+  @SubscribeEvent(receiveCanceled = true)
+  public static void onFogColorRender(EntityViewRenderEvent.FogColors event) {
+    if (event.getInfo().getEntity() instanceof PlayerEntity) {
+      PlayerEntity playerEntity = (PlayerEntity) event.getInfo().getEntity();
+      if (playerEntity.hasEffect(EffectRegistry.estrus.get())) {
+        event.setRed(((Constants.ESTRUS_COLOR_INT >> 16) & 0xff) / 255f);
+        event.setGreen(((Constants.ESTRUS_COLOR_INT >> 8) & 0xff) / 255f);
+        event.setBlue((Constants.ESTRUS_COLOR_INT & 0xff) / 255f);
+      }
+    }
+  }
+
+  @SubscribeEvent(receiveCanceled = true)
+  public static void onFogDensityRender(EntityViewRenderEvent.FogDensity event) {
+    if (event.getInfo().getEntity() instanceof PlayerEntity) {
+      PlayerEntity playerEntity = (PlayerEntity) event.getInfo().getEntity();
+      EffectInstance effect = playerEntity.getEffect(EffectRegistry.estrus.get());
+      if (effect != null) {
+        event.setDensity(0.1f * Math.min((float) effect.getDuration() / EstrusEffect.EFFECT_INTERVAL, 1));
+        event.setCanceled(true);
+      }
     }
   }
 }
